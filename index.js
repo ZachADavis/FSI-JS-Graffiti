329409a12dded6df5e4e22c1ec87ed0af02b2dc4
Zachary Davis <kicksilver@icloud.com>
2022-04-13 19:00:34 -0400

    Adjustments

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let header = document.querySelector("#page-header")
+header.style.textAlign = 'left'
 
+let dogImages = document.querySelectorAll('.dog-image')
+for( let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = '50px'
+}
+
+for( let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.transform = 'rotate(180deg)'
+}
 // Select some elements...
